fc21dbc42747bedb8b606550454b795e7d52aca7
Michael Blank <michael.blank@wework.com>
2018-02-11 20:01:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,13 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_riainbow(colors)
-  puts colors#{0}
+  puts "colors#{0}"
+  puts "colors#{1}"
+  puts "colors#{2}"
+  puts "colors#{3}"
+  puts "colors#{4}"
+  puts "colors#{5}"
+  puts "colors#{6}"
+end
+
+display_riainbow(colors)
